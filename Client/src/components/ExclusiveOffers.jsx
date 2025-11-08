f177fad3a9b4563543f8c898146b7c6f78304c13
itskelvinubuntuclear <kelvinmwarano4@gmail.com>
2025-11-08 11:32:34 +0300

    Made some modifications

--- Client/src/components/ExclusiveOffers.jsx
+++ Client/src/components/ExclusiveOffers.jsx
@@ -13,7 +13,7 @@ const ExclusiveOffers = () => {
             </button>
         </div>
 
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
             {exclusiveOffers.map((item)=>(
                 <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${item.image})`}}>
                     <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>{item.priceOff}% OFF</p>
@@ -25,7 +25,7 @@ const ExclusiveOffers = () => {
                     
                     <button className='flex items-center gap-2 font-medium cursor-pointer mt-4 mb-5'>
                         View Offers
-                        <img className='invert group-hover:translate-x-l translate-all' src={assets.arrowIcon} alt="arrow-icon" />
+                        <img className='invert group-hover:translate-x-1 transition-all' src={assets.arrowIcon} alt="arrow-icon" />
                     </button>
                 </div>
             ))}
