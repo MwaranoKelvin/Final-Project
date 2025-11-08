6d79d0e005013fa164f0b3e5711da561a5ddca03
itskelvinubuntuclear <kelvinmwarano4@gmail.com>
2025-11-08 12:08:26 +0300

    Completed the NewsLetter File

--- Client/src/components/StarRating.jsx
+++ Client/src/components/StarRating.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const StarRating = ({rating=4}) => {
+const StarRating = ({rating = 4}) => {
     return (
         <>
-            {Array(5).fill(0).map((_, index) => (
+            {Array(5).fill('').map((_, index) => (
                 <img src={rating > index ? assets.starIconFilled : assets.starIconOutlined} alt="star-icon" className='w-4.5 h-4.5' />
             ))}
 
