6915401cf73de6109ff8968cc4d740e479195466
itskelvinubuntuclear <kelvinmwarano4@gmail.com>
2025-11-10 08:37:52 +0300

    Modified the booking page.

--- Client/src/pages/RoomDetails.jsx
+++ Client/src/pages/RoomDetails.jsx
@@ -67,11 +67,31 @@ const RoomDetails = () => {
             </div>
 
             {/* Check-In Check-Out Form */}
-            <form className='flex flex-col md:flex-ro items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
-                <div>
+            <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
+                <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+                    <div className='flex flex-col'>
+                        <label htmlFor="checkInDate" className='font-medium'>Check-in</label>
+                        <input type="date" id='checkInDate' placeholder='Check-In'  className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                    </div>
+                    <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+
+                     <div className='flex flex-col'>
+                        <label htmlFor="checkOutDate" placeholder='Check-Out' className='font-medium'>Check-Out</label>
+                        <input type="date" id='checkOutDate' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                    </div>
+
+                    <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor="guests" className='font-medium'>Guests</label>
+                        <input type="number" placeholder='0' id='guests' className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                    </div>
+                    
                     
                 </div>
-
+                <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+                    Check Availability
+                </button>
             </form>
 
         </div>
