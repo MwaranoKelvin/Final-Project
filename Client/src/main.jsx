a87633c2109b6a6934575c0e08efcb957ec82a4e
itskelvinubuntuclear <kelvinmwarano4@gmail.com>
2025-11-20 23:06:35 +0300

    Changed the main.jsx file

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,25 +1,48 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { BrowserRouter } from 'react-router-dom'
+// import { ClerkProvider } from '@clerk/clerk-react'
+// import { AppProvider } from './context/AppContext.jsx'
+
+// // Import your Publishable Key
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error('Missing Publishable Key');
+// }
+
+// createRoot(document.getElementById('root')).render(
+//   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+//     <BrowserRouter>
+//       <AppProvider>
+//         <App />
+//       </AppProvider>
+
+//     </BrowserRouter>
+//   </ClerkProvider>,
+// )
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
 import { AppProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key');
+  throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-
     </BrowserRouter>
-  </ClerkProvider>,
+  </ClerkProvider>
 )
